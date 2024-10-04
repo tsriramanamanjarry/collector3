c5fc3450759f797b03454dbd67a072f1a47e4dd1
tsriramanamanjarry <143001940+tsriramanamanjarry@users.noreply.github.com>
2024-10-04 09:37:27 +0530

    Create collector3.java

---
+++ collector3.java
@@ -0,0 +1,17 @@
+package Demo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Example {
+
+	public static void main(String[] args) {
+		ArrayList <Float> c = new ArrayList<>();
+		  
+ c.add(23.4f);
+  
+ System.out.println(c);
+	}
+
+}
